174ff1f1dee0c572bb2fce551f465deb25322179
Jerwinn <Jerwin@live.co.uk>
2022-04-07 23:43:26 +0100

    Interface changes
    Changing interfaces

---
+++ lib/homePage.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double x = 0.0;
+  double y = 0.0;
+  double z = 0.0;
+  int steps = 0;
+  double distanceInMiles = 0.0;
+  double sessionTime = 0.0;
+  double calories = 0.0;
+  double addValue = 0.025;
+  double previousDistance = 0.0;
+  double distance = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
